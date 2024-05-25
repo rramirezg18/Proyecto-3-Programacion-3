a312c547e488789b51698da54810407f9a74d390
rramirezg18 <rramirezg18@miumg.edu.gt>
2024-05-24 20:34:06 -0600

    first commit

---
+++ src/main/java/com/mycompany/proyecto3programacion3/Proyecto3Programacion3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto3programacion3;
+
+/**
+ *
+ * @author ianto
+ */
+public class Proyecto3Programacion3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
